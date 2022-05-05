b33752a3c8650d5959090a994c63dffe3560da1a
sakshijain20 <jain.sakshi1099@gmail.com>
2022-05-05 16:36:01 +0530

    init commit

---
+++ src/test/java/blogapplication/blogapplication/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package blogapplication.blogapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
